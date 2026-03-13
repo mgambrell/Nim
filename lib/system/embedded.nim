4bc1286047c20370fcce3b9da39fcf27d1593332
mgambrell <mgambrell@users.noreply.github.com>
2026-03-13 03:40:11 -0400

    But not sure if we need it: Revert "some kind of wip thing for better exceptions on embedded platform" This reverts commit be2a5ef0636155211eb23db4b59711a3e12defa8.

--- lib/system/embedded.nim
+++ lib/system/embedded.nim
@@ -7,7 +7,8 @@
 #    distribution, for details about the copyright.
 #
 
-# Standalone implementation with working exception handling.
+
+# Bare-bones implementation of some things for embedded targets.
 
 proc chckIndx(i, a, b: int): int {.inline, compilerproc.}
 proc chckRange(i, a, b: int): int {.inline, compilerproc.}
@@ -21,33 +22,9 @@ proc setFrame(s: PFrame) {.compilerRtl, inl.} = discard
 when not gotoBasedExceptions:
   proc pushSafePoint(s: PSafePoint) {.compilerRtl, inl.} = discard
   proc popSafePoint {.compilerRtl, inl.} = discard
-
-var currException {.threadvar.}: ref Exception
-
-when gotoBasedExceptions:
-  var nimInErrorMode {.threadvar.}: bool
-
-proc pushCurrentException(e: sink(ref Exception)) {.compilerRtl, inl.} =
-  e.up = currException
-  currException = e
-
-proc popCurrentException {.compilerRtl, inl.} =
-  currException = currException.up
-
-proc closureIterSetExc(e: ref Exception) {.compilerRtl, inl.} =
-  currException = e
-
-proc nimBorrowCurrentException(): ref Exception {.compilerRtl, inl, benign, nodestroy.} =
-  result = currException
-
-proc getCurrentException*(): ref Exception {.compilerRtl, inl, benign.} =
-  result = currException
-
-proc getCurrentExceptionMsg*(): string {.inline, benign.} =
-  return if currException == nil: "" else: currException.msg
-
-proc setCurrentException*(exc: ref Exception) {.inline, benign.} =
-  currException = exc
+proc pushCurrentException(e: ref Exception) {.compilerRtl, inl.} = discard
+proc popCurrentException {.compilerRtl, inl.} = discard
+proc closureIterSetExc(e: ref Exception) {.compilerRtl, inl.} = discard
 
 # some platforms have native support for stack traces:
 const
@@ -56,30 +33,18 @@ const
 
 proc quitOrDebug() {.noreturn, importc: "abort", header: "<stdlib.h>", nodecl.}
 
-proc raiseExceptionAux(e: sink(ref Exception)) {.nodestroy.} =
-  pushCurrentException(e)
-  when gotoBasedExceptions:
-    inc nimInErrorMode
+proc raiseException(e: ref Exception, ename: cstring) {.compilerRtl.} =
+  sysFatal(ReraiseDefect, "exception handling is not available")
 
 proc raiseExceptionEx(e: sink(ref Exception), ename, procname, filename: cstring,
-                      line: int) {.compilerRtl, nodestroy.} =
-  if e.name.isNil: e.name = ename
-  raiseExceptionAux(e)
-
-proc raiseException(e: sink(ref Exception), ename: cstring) {.compilerRtl.} =
-  raiseExceptionEx(e, ename, nil, nil, 0)
+                      line: int) {.compilerRtl.} =
+  sysFatal(ReraiseDefect, "exception handling is not available")
 
 proc reraiseException() {.compilerRtl.} =
-  if currException == nil:
-    sysFatal(ReraiseDefect, "no exception to reraise")
-  else:
-    when gotoBasedExceptions:
-      inc nimInErrorMode
-    else:
-      raiseExceptionAux(currException)
+  sysFatal(ReraiseDefect, "no exception to reraise")
 
 proc raiseDefect() {.compilerRtl.} =
-  rawQuit(1)
+  sysFatal(ReraiseDefect, "exception handling is not available")
 
 proc writeStackTrace() = discard
 
@@ -87,10 +52,11 @@ proc unsetControlCHook() = discard
 proc setControlCHook(hook: proc () {.noconv.}) = discard
 
 when gotoBasedExceptions:
+  var nimInErrorMode {.threadvar.}: bool
+
   proc nimErrorFlag(): ptr bool {.compilerRtl, inl.} =
     result = addr(nimInErrorMode)
 
   proc nimTestErrorFlag() {.compilerRtl.} =
-    if nimInErrorMode and currException != nil:
-      currException = nil
-      rawQuit(1)
+    if nimInErrorMode:
+      sysFatal(ReraiseDefect, "exception handling is not available")
